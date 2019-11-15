58aab9a9f63a8e06ed96e186178de465eab485ca
Caio Coutinho <caio@hlp.com.br>
2019-11-15 17:56:59 -0300

    Campos de validação

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -3,10 +3,11 @@ using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
@@ -21,10 +22,22 @@ namespace QuickBuy.Dominio.Entidades
         public FormaPagamento FormaPagamento { get; set; }
 
 
+
         /// <summary>
         /// Deve ter pelo menos 1 pedido ou muitos pedidos
         /// </summary>
         public ICollection<ItemPedido> ItensPedido { get; set; }
+
+        public override void Validate()
+        {
+            LimparMensagensValidacao();
+            if (!ItensPedido.Any()) {
+                AdicionarCritica("Critica - Pedido não pode ficar sem item");
+                if (string.IsNullOrEmpty(CEP)) {
+                    AdicionarCritica("Obrigatorio - O campo CEP não esta preenchido");
+                }
+            }
+        }
     }
 
 }
